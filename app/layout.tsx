7a024d77ab429ab5de28d4e4a1616c07f4e40533
drjvtlkr <dheerajrvithalkar@gmail.com>
2024-11-29 01:38:15 +0530

    about page done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Roboto } from "@next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
