6d7de61827ea9928c10d0fa5b682056fde274932
drjvtlkr <dheerajrvithalkar@gmail.com>
2024-11-29 15:47:18 +0530

    transiton and other imporvements

--- components/Cursor.tsx
+++ components/Cursor.tsx
@@ -1,39 +1,54 @@
-'use client'
+"use client";
 import React, { useEffect } from "react";
-import {gsap} from 'gsap'
+import { gsap } from "gsap";
 
 const Cursor = () => {
-    useEffect(()=>{
-        const cursor = document.getElementById('custom-cursor')
-        const links = document.querySelectorAll('a')
-        const cursorText = document.querySelector('.cursor-text') as HTMLElement;
+  useEffect(() => {
+    const cursor = document.getElementById("custom-cursor");
+    const links = document.querySelectorAll("a");
+    const cursorText = document.querySelector(".cursor-text") as HTMLElement;
+    const centerX = window.innerWidth/2;
+    const centerY = window.innerHeight/2;
 
-        const onMouseMove = (event: MouseEvent)=>{
-            const {clientX, clientY}= event;
-            gsap.set(cursor,{x: clientX, y: clientY})
-        }
+    const cursorTransitionDuration = 0.25;
 
-        const onMouseEnterLink=(event :MouseEvent)=>{
-            const link = event.target as HTMLElement;
-            if (link.classList.contains('view')) {
-                gsap.set(cursor,{scale:4})
-                cursorText.style.display='block'
-            }else{
-                gsap.set(cursor,{scale:4})
-            }
-        }
+    const onMouseMove = (event: MouseEvent) => {
+      const { clientX, clientY } = event;
+      gsap.to(cursor, { x: clientX, y: clientY, duration: cursorTransitionDuration, ease:"power2.out"});
+    };
 
-        const onMouseLeaveLink=()=>{
-            gsap.set(cursor,{scale:4})
-            cursorText.style.display='none'
-        }
+    const onMouseEnterLink = (event: MouseEvent) => {
+      const link = event.target as HTMLElement;
+      if (link.classList.contains("view")) {
+        gsap.to(cursor, { scale: 4 ,duration: cursorTransitionDuration, ease:"power2.out"});
+        cursorText.style.display = "block";
+      } else {
+        gsap.to(cursor, { scale: 4, duration: cursorTransitionDuration,ease:"power2.out" });
+      }
+    };
 
-        document.addEventListener('mousemove', onMouseMove)
-        links.forEach((link)=>{
-            link.addEventListener('mouseenter', onMouseEnterLink)
-            link.addEventListener('mouseleave', onMouseLeaveLink)
-        })
-    })
+    const onMouseLeaveLink = () => {
+      gsap.to(cursor, { scale: 4, duration: cursorTransitionDuration, ease:"power2.out" });
+      cursorText.style.display = "none";
+    };
+
+    const onWindowBlur =()=>{
+        gsap.to(cursor, {duration:cursorTransitionDuration, x:centerX, y:centerY,ease:"power2.out"})
+    }
+
+    const onWindowFocus = ()=>{
+        gsap.to(cursor,{duration:cursorTransitionDuration, scale:1, ease:"power2.out"})
+    }
+
+    document.addEventListener("mousemove", onMouseMove);
+    links.forEach((link) => {
+      link.addEventListener("mouseenter", onMouseEnterLink);
+      link.addEventListener("mouseleave", onMouseLeaveLink);
+    });
+    window.addEventListener("blur", onWindowBlur);
+    window.addEventListener("focus", onWindowFocus)
+  });
+
   return (
     <div id="custom-cursor" className="custom-cursor">
       <span className="cursor-text">Cursor</span>
